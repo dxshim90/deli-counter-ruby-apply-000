f0a6883410508ab512c9c72553fe71342a7ce8cc
Daniel Shimield <dxshim90@gmail.com>
2019-01-10 11:58:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,9 +11,10 @@ def line(katz_deli)
   if katz_deli.empty? == true
     puts "The line is currently empty."
   else
+    the_line = "The line is currently:"
     katz_deli.each_with_index do |customer, number|
-      current_line.push(number, customer)
-    puts "The line is currently:" 
+      the_line.push(number+1, customer)
+      puts 
     end
     
     def now_serving(katz_deli)
