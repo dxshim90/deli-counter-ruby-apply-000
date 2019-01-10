edd8b7b2d13652b8059abda40a9efcd16ccabded
Daniel Shimield <dxshim90@gmail.com>
2019-01-10 12:32:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,28 +3,28 @@
 katz_deli = []
 
 def take_a_number(deli, name)
-katz_deli << (name)
-place = katz_deli.index(name)
+deli << (name)
+place = deli.index(name)
 puts "Welcome, #{name}. You are number #{place+1} in line."
 end
 
 
 
 def line(deli)
-if katz_deli.empty? == true
+if deli.empty? == true
 puts "The line is currently empty."
 else
-katz_deli.each do |customer, position|
+deli.each do |customer, position|
 puts "The line is #{poition+1} #{customer}"
 end
 end
 
-def now_serving(katz_deli)
-if katz_deli.empty? == true
+def now_serving(deli)
+if deli.empty? == true
 puts "There is nobody waiting to be served!"
 else
-puts "Currently serving #{katz_deli.first}."
-katz_deli.shift
+puts "Currently serving #{deli.first}."
+deli.shift
 end
 end
 end
