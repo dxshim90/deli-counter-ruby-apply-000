011e2392b7a12a0ca69ba8442d153712b70bce70
Daniel Shimield <dxshim90@gmail.com>
2019-01-10 12:47:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,7 +16,7 @@ puts "The line is currently empty."
 else
   the_line = ""
 deli.each do |customer, position|
-  the_line << "#{}. #{customer}"
+  the_line << "#{position+1}. #{customer}"
 end
 puts "The line is currently #{the_line}"
 end
