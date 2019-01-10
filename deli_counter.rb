1db7653f7450ad6790fa2ef3387edd8e7b275629
Daniel Shimield <dxshim90@gmail.com>
2019-01-10 12:41:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,7 +14,7 @@ def line(deli)
 if deli.empty? == true
 puts "The line is currently empty."
 else
-  the_line = " "
+  the_line = ""
 deli.each do |customer, position|
   the_line << "#{position + 1}. #{customer}"
 end
